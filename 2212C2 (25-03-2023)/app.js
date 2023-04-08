869746ea1df5d631f052182afe5e70a6cba4961d
Sameer Faisal <45840879+SameerFaisal@users.noreply.github.com>
2023-04-08 10:39:53 +0500

    Update app.js

---
+++ 2212C2 (25-03-2023)/app.js
@@ -0,0 +1,41 @@
+// let sentence=prompt("Enter a sentence")
+// document.write("Original Sentence: "+sentence)
+// let lastIndex=sentence.length-1
+// let letter=sentence[lastIndex]
+// document.write("<br>")
+// if(letter.toLowerCase()==="a"||letter.toLowerCase()==="e"||letter.toLowerCase()==="i"||letter.toLowerCase()==="o"||letter.toLowerCase()==="u"){
+//     document.write(letter+" is a vowel")
+// }
+// else{
+//     document.write(letter+" is a consonant")
+// }
+
+// let sentence=prompt("Enter a sentence")
+// let letter=sentence[sentence.length-1]
+// alert(letter)
+// if(letter.toLowerCase()==="a"||letter.toLowerCase()==="e"||letter.toLowerCase()==="i"||letter.toLowerCase()==="o"||letter.toLowerCase()==="u"){
+//     document.write(letter+" is a vowel")
+// }
+// else{
+//     document.write(letter+" is a consonant")
+// }
+// const fruits = ["Banana", "Orange", "Apple", "Mango"]
+// fruits.sort().reverse()
+// document.write(fruits)
+
+// let nums=[]
+
+// let howMany=+prompt("Enter how many values you want to enter")
+// for(let i=0;i<howMany;i++){
+//     nums[i]=parseFloat(prompt("Enter "+i+"th value"))
+// }
+// nums.sort().reverse()
+// for(let j in nums){
+//     document.write(nums[j]+"<br>")
+// }
+
+
+// nums.sort().reverse()
+// document.write(nums)
+
+
